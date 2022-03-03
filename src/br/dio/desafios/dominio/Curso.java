f86c950e063d3ec90aa1d34444dcd8c10a6cf123
Allcidez <allcidez@yahoo.com.br>
2022-03-03 20:44:11 -0300

    testes de commit 3

--- src/br/dio/desafios/dominio/Curso.java
+++ src/br/dio/desafios/dominio/Curso.java
@@ -1,35 +1,67 @@
 package br.dio.desafios.dominio;
 
+import java.util.Objects;
+
 public class Curso {
 	private String titutlo;
 	private String descricao;
 	private int cargaHoraria;
-	
+
 	public Curso() {
-		
+
 	}
-	
+
+	public Curso(String titutlo, String descricao, int cargaHoraria) {
+		this.titutlo = titutlo;
+		this.descricao = descricao;
+		this.cargaHoraria = cargaHoraria;
+	}
+
 	public String getTitutlo() {
 		return titutlo;
 	}
-	
+
 	public void setTitutlo(String titutlo) {
 		this.titutlo = titutlo;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
-	
+
 	public void setCargaHoraria(int cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Curso [titutlo=" + titutlo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cargaHoraria, descricao, titutlo);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		return cargaHoraria == other.cargaHoraria && Objects.equals(descricao, other.descricao)
+				&& Objects.equals(titutlo, other.titutlo);
+	}
+
 }
--- src/br/dio/desafios/dominio/Mentoria.java
+++ src/br/dio/desafios/dominio/Mentoria.java
@@ -10,33 +10,47 @@ public class Mentoria {
 	public Mentoria(){}
 	
 	
+	public Mentoria(String titutlo, String descricao, LocalDate data) {
+		super();
+		this.titutlo = titutlo;
+		this.descricao = descricao;
+		this.data = data;
+	}
+
+
 	public String getTitutlo() {
 		return titutlo;
 	}
+
+
 	public void setTitutlo(String titutlo) {
 		this.titutlo = titutlo;
 	}
-	
+
+
 	public String getDescricao() {
 		return descricao;
 	}
+
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
+
 	public LocalDate getData() {
 		return data;
 	}
+
+
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
-	
-	
-	//@Override 
-	//public String toString() {
-	//	return "Curso{" + "Crusso
-	//	
-	//}
-	
-	
+
+
+	@Override
+	public String toString() {
+		return "Mentoria [titutlo=" + titutlo + ", descricao=" + descricao + ", data=" + data + "]";
+	}
+		
 }
